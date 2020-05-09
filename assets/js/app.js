cf7346716705403ec7a619cad4327b93739e78e6
iddrisu sumaila <sumaila129@gmail.com>
2020-05-09 13:59:08 -0700

    still work in progress

--- assets/js/app.js
+++ assets/js/app.js
@@ -47,6 +47,7 @@ function showSlides(n) {
   // for (i = 0; i < dots.length; i++) {
   //   dots[i].className = dots[i].className.replace(" active", "");
   // }
+  console.log(slides[slideIndex-1]);
   slides[slideIndex-1].style.display = "block";
   // dots[slideIndex-1].className += " active";
   // captionText.innerHTML = dots[slideIndex-1].alt;
